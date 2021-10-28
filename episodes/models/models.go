b357ead05e1fa90d0078d37e7cddb4ba71bfbe46
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-10-28 18:52:46 -0300

    Added json tags to models

--- episodes/models/models.go
+++ episodes/models/models.go
@@ -3,9 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Episode struct {
-	ID          primitive.ObjectID `bson:"id,omitempty"`
-	Serie       primitive.ObjectID `bson:"serie,omitempty"`
-	Title       string             `bson:"title,omitempty"`
-	Description string             `bson:"description,omitempty"`
-	Duration    int32              `bson:"duration,omitempty"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Serie       primitive.ObjectID `json:"serie,omitempty" bson:"serie,omitempty"`
+	Title       string             `json:"title,omitempty" bson:"title,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+	Duration    int32              `json:"duration,omitempty" bson:"duration,omitempty"`
 }
--- series/models/models.go
+++ series/models/models.go
@@ -3,8 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Series struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Title    string             `bson:"title,omitempty"`
-	Category string             `bson:"category,omitempty"`
-	Cast     []string           `bson:"cast,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title    string             `json:"title,omitempty" bson:"title,omitempty"`
+	Category string             `json:"category,omitempty" bson:"category,omitempty"`
+	Cast     []string           `json:"cast,omitempty" bson:"cast,omitempty"`
 }
