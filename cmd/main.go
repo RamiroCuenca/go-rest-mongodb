fa37aac04828351a5d0cd43b99dd9cfae9ce91c1
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-02 08:22:30 -0300

    Modified main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/RamiroCuenca/go-rest-mongodb/database/connection"
 )
 
 func main() {
 	// Get mongo client
-	mongoClient := connection.GetMongoClient()
+	// MongoClient := connection.GetMongoClient()
 
-	// Set the database object
-	db := mongoClient.Database("crud-database")
+	// // Set the database object
+	// DB := MongoClient.Database("crud-database")
 
-	// Set series and episodes collections
-	seriesCollection := db.Collection("series")
-	episodesCollection := db.Collection("episodes")
+	// // Set series and episodes collections
+	// seriesCollection := db.Collection("series")
+	// episodesCollection := db.Collection("episodes")
 
 	// Get router
 	mux := GetRouter()
 
 	// Run locally
 	http.ListenAndServe(":8000", mux)
-
-	fmt.Println(db, seriesCollection, episodesCollection)
 }
