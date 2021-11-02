26b8cfb02f2862d4206fd827adf95fa33543baec
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-02 08:22:09 -0300

    Created a route for Create handler

--- cmd/router.go
+++ cmd/router.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	series "github.com/RamiroCuenca/go-rest-mongodb/series/controllers"
+	"github.com/gorilla/mux"
+)
 
 func GetRouter() *mux.Router {
+	// Set up the router
 	mux := mux.NewRouter()
+
+	// Handlers
+	mux.HandleFunc("/series", series.Create).Methods("POST")
 
 	return mux
 }
