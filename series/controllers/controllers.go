122b1c02b6b7ad43d4213b669ddb28ef4b05fdde
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-02 16:18:42 -0300

    Create a handler to getAll series

--- series/controllers/controllers.go
+++ series/controllers/controllers.go
@@ -3,12 +3,14 @@ package controllers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/RamiroCuenca/go-rest-mongodb/common"
 	"github.com/RamiroCuenca/go-rest-mongodb/database/connection"
 	"github.com/RamiroCuenca/go-rest-mongodb/series/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Create a new serie
@@ -34,6 +36,8 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	// 4. InsertOne document - result will contain InsertedID int
 	seriesCollection.InsertOne(ctx, serie)
 
+	// result, err := seriesCollection.InsertOne(ctx, serie)
+
 	// serie.ID = result.InsertedID // Does not work :(
 
 	// 5. Encode the result (Contains the id)
@@ -43,4 +47,55 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	// 6. Send response
 	common.SendResponse(w, http.StatusOK, []byte(json))
 
+}
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	// 1. Connect to the database and setup collection variable
+	// 2. Create a Series Array where we can store data
+	// 3. Setup context
+	// 4. Execute the query (Remember to close cursor)
+	// 5. Insert data from cursor on array
+	// 6. Send response
+
+	// 1.
+	db := connection.GetMongoClient()
+	collection := db.Database("crud-database").Collection("series")
+
+	// 2.
+	var seriesArr []models.Series
+
+	// 3.
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// 4.
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		common.SendError(w, http.StatusInternalServerError, []byte(`{"message": `+err.Error()+`}`))
+		return
+	}
+	defer cursor.Close(ctx)
+
+	// 5.
+	for cursor.Next(ctx) {
+		var serie models.Series
+
+		cursor.Decode(&serie)
+
+		seriesArr = append(seriesArr, serie)
+	}
+
+	if err = cursor.Err(); err != nil {
+		common.SendError(w, http.StatusInternalServerError, []byte(`{"message": `+err.Error()+`}`))
+		return
+	}
+
+	// 6.
+	// json.NewEncoder(w).Encode(seriesArr)
+	json, err := json.Marshal(seriesArr)
+
+	resp := fmt.Sprintf(`{
+		"result": %s
+	}`, json)
+
+	common.SendResponse(w, http.StatusOK, []byte(resp))
 }
